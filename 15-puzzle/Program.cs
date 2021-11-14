4e6469ce7446e1e3ad522b5615a04e36f46d7f02
Karolina Maciejewska <kmaciejewska14@gmail.com>
2021-11-14 20:46:32 +0100

    Add IDDFS implementation

--- 15-puzzle/Program.cs
+++ 15-puzzle/Program.cs
@@ -12,11 +12,11 @@ namespace _15Puzzle
     {
         static void Main(string[] args)
         {
-            var arr = new int[3, 3] { { 1, 2, 5 }, { 3, 4, 0 }, { 6, 7, 8 } };
+            var arr = new int[3, 3] { { 1, 2, 5 }, { 3, 0, 4 }, { 6, 7, 8 } };
 
             Board initPuzzle = new Board(arr);
             var startingState = new BoardState(initPuzzle, null, null);
-            Solver bfs = new DFS();
+            Solver bfs = new BFS();
 
             bfs.Solve(startingState);
 
---
+++ 15-puzzle/solvers/IDFS.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15_puzzle.solvers
+{
+    public class IDFS : Solver
+    {
+        public override void Solve(BoardState root)
+        {
+            int depth = 1; 
+            for(int i = 0; i <= depth; i++)
+            {
+                var foundRemaining = DLS(root, i);
+                if (foundRemaining.Item1 != null)
+                {
+                    Console.WriteLine("Solved!");
+                    //trace path to root node 
+                    this.PrintSolution(foundRemaining.Item1);
+                    break;
+                }
+                else if (!foundRemaining.Item2)
+                {
+                    return;
+                }
+                depth++;
+            }
+
+        }
+
+        public Tuple<BoardState, bool> DLS(BoardState root, int limit)
+        {
+            // If reached the maximum depth, stop recursing.
+            if (limit <= 0)
+            {
+                if (root.currentBoard.IsEqual(this.GoalState))
+                    return new Tuple<BoardState, bool>(root, true);
+                else
+                    return new Tuple<BoardState, bool>(null, true);
+            }
+
+            bool remaining = false;
+
+            //recur for all the vertices adjacent to source vertex
+            var zero = root.currentBoard.IndexOfZero();
+            var zeroX = zero.Item1;
+            var zeroY = zero.Item2;
+
+            var children = this.ExpandBoard(root, zeroX, zeroY);
+
+            for (int i = 0; i < children.Count(); i++)
+            {
+                Tuple<BoardState, bool> foundRemaining = DLS(children[i], limit - 1);
+                if (foundRemaining.Item1 != null)
+                    return new Tuple<BoardState, bool>(foundRemaining.Item1, true);
+                if (foundRemaining.Item2)
+                    remaining = true;
+            }
+            return new Tuple<BoardState, bool>(null, remaining);
+        }
+    }
+}
