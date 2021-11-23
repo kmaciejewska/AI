fcc7222253f2841733a355fc47d8ef069f061920
Karolina Maciejewska <kmaciejewska14@gmail.com>
2021-11-23 09:16:09 +0100

    Start BF implementation

---
+++ 15-puzzle/solvers/BF.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using C5;
+
+namespace _15_puzzle.solvers
+{
+    public class BF : Solver
+    {
+        public override void Solve(BoardState root)
+        {
+            var queue = new IntervalHeap<BoardState>(); //last in first out
+            HashSet<Board> visited = new HashSet<Board>();
+
+            stack.Push(root);
+            visited.Add(root.currentBoard);
+
+            while (stack.Count > 0)
+        }
+    }
+}
